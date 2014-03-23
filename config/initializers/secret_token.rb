3a868fd27663dc51b223daeeae7452c1ff941312
Linkon Islam <linkondhk@gmail.com>
2014-03-23 20:17:37 +0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lurms::Application.config.secret_key_base = '6057b5e9682b6eaeee0302b38b989509dc453a9633f8ff2eb80bb4c80dc080da0dc9d205011029af48e61d2ad7aa9d41d3a918bdc81e4cce94f853d5d33de179'
